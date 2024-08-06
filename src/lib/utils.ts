9c7dedcbf337e40e3b28a7266ca550b621942c0d
Pradyumn Dubey <161572258+pdubeyagra@users.noreply.github.com>
2024-08-06 11:39:22 +0530

    Update utils.ts 1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
- export function absoluteUrl(path: string) {
-return `$(process.env.NEXT_PUBLIC_APP_URL)${path}`
- }
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
+}
 
 // export const absoluteUrl = (path = "") => {
 //  const baseUrl = process.env.NEXT_PUBLIC_APP_URL;
